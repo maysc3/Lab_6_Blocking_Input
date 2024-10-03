e97b8fa7a6cbda8901166027829546bd6dc5c696
Corey Mays <corey.mays17@gmail.com>
2024-10-03 12:06:32 -0400

    CtoFConverter

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,7 +1,40 @@
-public class CtoFConverter
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class CtoFConverter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double cTemp = 0;
+        double fTemp = 0;
+        String trash = "";
+        String continueYN = "";
+        boolean tempDone = false;
+        boolean conversions = false;
+
+        do
+        {
+            do
+            {
+                System.out.print("Enter a valid temperature in celsius: ");
+
+                if (in.hasNextDouble()) {
+                    cTemp = in.nextDouble();
+                    in.nextLine(); // clears buffer
+                    tempDone = true;
+                } else {
+                    trash = in.nextLine();
+                }
+            }
+            while (!tempDone);
+            fTemp = (9.0 / 5.0 * cTemp) + 32;
+            System.out.println("The temperature " + cTemp + "C in fahrenheit is " + fTemp + "F");
+            System.out.println("Do you have another conversion? [Y/N]");
+            if (in.hasNextLine())
+            {
+                continueYN = in.nextLine();
+                if (continueYN.equalsIgnoreCase("N"))
+                    conversions = true;
+            }
+        }
+        while (!conversions);
     }
 }
