3b93f90b084892442155d1e23bb7f01bdb5f0baa
Corey Mays <corey.mays17@gmail.com>
2024-10-03 21:01:56 -0400

    final

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,7 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
 public class HighorLow
 {
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rng = new Random();
+        String trash = "";
+        String continueYN = ""
+;        int guess = 0;
+        boolean Gdone = false;
+        boolean done = false;
+        do {
+            int val = rng.nextInt(10) + 1; // generates random val
+            do {
+                System.out.print("Enter a number [1-10]: ");
+                if (in.hasNextInt()) {
+                    guess = in.nextInt();
+                    in.nextLine();
+                    if (guess >= 1 && guess <= 10) {
+                        Gdone = true;
+                    }
+                } else {
+                    trash = in.nextLine();
+                }
+            } while (!Gdone);
+            System.out.println("The Lucky value was: " + val);
+            if (val == guess) {
+                System.out.println("You were right on the money! ");
+            } else if (guess < val) {
+                System.out.println("You're guess was low :( ");
+            } else {
+                System.out.println("You're guess was too high :( ");
+            }
+            System.out.print("Guess again? [Y/N]: ");
+            if(in.hasNextLine())
+            {
+                continueYN = in.nextLine();
+                if (continueYN.equalsIgnoreCase("N")) {
+                    done = true;
+                }
+            }
+        }while(!done);
     }
 }
