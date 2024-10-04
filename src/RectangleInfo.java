ba1d5ac28ce8ff4fcb733b395a74deca2592292e
Corey <corey.mays17@gmail.com>
2024-10-03 21:18:35 -0400

    rectanlge fixed

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -6,6 +6,8 @@ public class RectangleInfo
         Scanner in = new Scanner(System.in);
         double width = 0;
         double length = 0;
+        double area = 0;
+        double perimeter = 0;
         double diagonal = 0;
         String trash = "";
         boolean doneW = false;
@@ -14,7 +16,7 @@ public class RectangleInfo
 
         do
         {
-            System.out.print("Enter the width of the triangle: ");
+            System.out.print("Enter the width of the rectangle: ");
             if(in.hasNextDouble())
             {
                 width = in.nextDouble();
@@ -30,7 +32,7 @@ public class RectangleInfo
             }
         }while(!doneW);
         do
-        {   System.out.print("Enter the length of the triangle: ");
+        {   System.out.print("Enter the length of the rectangle: ");
             if(in.hasNextDouble())
             {
                 length = in.nextDouble();
@@ -45,7 +47,11 @@ public class RectangleInfo
                 trash = in.nextLine();
             }
         }while(!doneL);
+        area = length * width;
+        perimeter = 2*(length + width);
         diagonal = Math.sqrt(Math.pow(width,2)+Math.pow(length,2));
+        System.out.println("The are of the rectangle is " + area);
+        System.out.println("The perimeter of the rectangle is " + perimeter);
         System.out.println("The length of the diagonal is " + diagonal);
     }
 }
